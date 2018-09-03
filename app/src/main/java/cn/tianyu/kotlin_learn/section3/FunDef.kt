e85a319fb5e38043debad59a98a71861e2ecd462
smartty <zhuty@xcbobo.com>
2018-09-03 15:38:54 +0800

    section3 finished 局部函数

--- app/src/main/java/cn/tianyu/kotlin_learn/section3/FunDef.kt
+++ app/src/main/java/cn/tianyu/kotlin_learn/section3/FunDef.kt
@@ -59,3 +59,22 @@ fun parsePathInRegex(path: String){
         LogUtil.d("parsePath", "Dir: $directory, name: $fileName, ext: $extension")
     }
 }
+
+/*
+    3.6
+ */
+class User(val id: Int, val name: String, val address: String)
+
+fun saveUser(user: User) {
+    user.validateBeforeSave()
+}
+
+fun User.validateBeforeSave() {
+    fun validate(value:String, fieldName: String){
+        if (value.isEmpty()) {
+            throw IllegalArgumentException("Can't save user ${id}: empty $fieldName")
+        }
+    }
+    validate(name, "Name")
+    validate(address, "Address")
+}
